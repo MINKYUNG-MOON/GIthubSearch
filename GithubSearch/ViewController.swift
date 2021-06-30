0c03b13df1ea32be7a84fc7d8bfaa0f89cef5dcf
moonriver <moonriver971205@gmail.com>
2021-06-30 22:07:04 +0900

    ui exercise

--- GithubSearch/ViewController.swift
+++ GithubSearch/ViewController.swift
@@ -2,18 +2,47 @@
 //  ViewController.swift
 //  GithubSearch
 //
-//  Created by 이동현 on 2021/06/30.
+//  Created by moonriver on 2021/06/30.
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+
+    let label = UILabel()
+    let button = UIButton()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
+        view.addSubview(label)
+
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        label.text =  "hello world"
+        label.textColor = .blue
+
+        view.addSubview(button)
+
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview().inset(100)
+
+            make.size.equalTo(100)
+
+        }
+
+        button.setTitle("클릭", for: .normal)
+        button.backgroundColor = .brown
+
+
+
     }
 
 
 }
-
